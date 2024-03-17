18ba7cb053f34dece84a91c4758cf60b916fc72c
unknown <esthermutuan@gmail.com>
2024-03-17 22:46:10 +0300

    a floder

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() { 
+  String school="Central primary schhol";
+  num year =2024;
+  int total=300;
+  double GPA= 3.9;
+  bool ispassed= false;
+print(school);
+print(year);
+print(total);
+print(GPA);
+print(ispassed);
+   print("Hello World!"); 
+}
