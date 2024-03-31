1572c71fba76c10759a16b74b54b0e74230aed01
unknown <esthermutuan@gmail.com>
2024-03-31 19:25:32 +0300

    esther portfolio

---
+++ JAVASCRIPT/Prac.js
@@ -0,0 +1,18 @@
+//ARITHMETIC OPERATORS
+//+ADDITION(+)
+//SUBRACTION(-)
+//MULTIPLICATION(*)
+//DIVISION(/)
+//EXPONENTION(**)
+//INCREMENT(++)
+//DECREMENT(--)
+//REMAINDER(%)
+
+let x = 10;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x ** y);
---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,35 @@
+//console.log("Hello world");
+
+//DEFINING VARIABLES IN JAVASCRIPT
+//1. USING THE VAR KEYWORD
+var name= 'Student Esther';//STRING DATATYPE
+console (name);
+
+//Using the let keyword
+let job='A PLP STUDENT';
+console.log(job);
+
+//USING THE CONSTANT KEYWORD TO DEFINE VARIABLES THAT DO NOT CHANGE
+const PI= 3.14152;// FLOATING NUMBER
+console.log(PI);
+
+// INTEGER DATATYPE
+var age= 29;
+
+//NAMING CONVETION
+// 1.USING THE CamelCase by having the first letter small and the second capital as shows
+firstName;
+// SNAKE CASE
+var last_name;// NB JAVASCRIPT IS CASE SENSITIVE
+
+//BOOLEN DATATYPE
+
+var success= false;
+
+/// NULL DATATYPE
+var balance= null;
+
+//UNDEFINED DATATYPE
+var lastname;
+
+
